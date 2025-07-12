0babccd2e1cc8de5df8adae58fae805740fb87da
SalahJamal1 <salahabufarha1996@outlook.com>
2025-07-12 10:14:23 +0300

    initial commit

--- client/app/_components/SignUpFrom.js
+++ client/app/_components/SignUpFrom.js
@@ -1,99 +1,147 @@
 "use client";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { signup } from "../_lib/apiResto";
 import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { SET_ERROR } from "../_store/userSlice";
 
 function SignUpFrom() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setfirstName] = useState("");
-  const [lastName, setlastName] = useState("");
-  const [address, setAddress] = useState("");
-  const [phone, setPhone] = useState("");
+  const { errors } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const [formData, setData] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    phone: "",
+    email: "",
+    password: "",
+  });
   const router = useRouter();
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const isValid = () => {
+    let tempError = {};
+    if (!formData.firstName.trim())
+      tempError.firstName = "please enter the First Name";
+    if (!formData.lastName.trim())
+      tempError.lastName = "please enter the Last Name";
+    if (!formData.address.trim())
+      tempError.address = "please enter the Address";
+    if (!formData.email.trim()) tempError.email = "please enter the Email";
+    if (!formData.phone.trim()) tempError.phone = "please enter the Phone";
+    if (!formData.password.trim())
+      tempError.password = "please enter the password";
+    Object.entries(tempError).forEach(([key, value]) => {
+      dispatch(SET_ERROR({ type: `ERROR_${key.toUpperCase()}`, value }));
+    });
+    return Object.entries(tempError).length === 0;
+  };
   async function handelSubmit(e) {
     e.preventDefault();
 
-    if (!email && !password && !firstName && !lastName && !address && !phone)
-      return;
-    const res = await signup({
-      firstName,
-      lastName,
-      address,
-      phone,
-      email,
-      password,
-    });
-    if (res.data.user) router.push("/login");
+    if (!isValid()) return;
+    try {
+      const res = await signup(formData);
+      if (res.data.user) router.push("/login");
+    } catch (err) {
+      const message = err?.response?.data?.message || err?.message;
+      toast.error(message);
+    }
   }
+
   return (
     <form
       method="POST"
       className="grid grid-cols-2 gap-x-14"
       onSubmit={handelSubmit}
     >
-      <div>
+      <div className="mb-3">
         <label className="block capitalize text-xl tracking-widest mb-2">
           First Name
         </label>
         <input
           type="text"
-          className="block mb-4 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
+          className="block mb-2 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
           placeholder="FirstName"
-          value={firstName}
-          onChange={(e) => setfirstName(e.target.value)}
+          name="firstName"
+          value={formData.firstName}
+          onChange={onChange}
         />
+        {errors.firstName && (
+          <p className="text-red-500 capitalize">{errors.firstName}</p>
+        )}
       </div>
-      <div>
+      <div className="mb-3">
         <label className="block capitalize text-xl tracking-widest mb-2">
           last Name
         </label>
         <input
           type="text"
-          className="block mb-4 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
+          className="block mb-1 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
           placeholder="LastName"
-          value={lastName}
-          onChange={(e) => setlastName(e.target.value)}
+          name="lastName"
+          value={formData.lastName}
+          onChange={onChange}
         />
+        {errors.lastName && (
+          <p className="text-red-500 capitalize">{errors.lastName}</p>
+        )}
       </div>
-      <div>
+      <div className="mb-3">
         <label className="block capitalize text-xl tracking-widest mb-2">
           address
         </label>
         <input
           type="text"
-          className="block mb-4 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
+          className="block mb-1 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
           placeholder="Address"
-          required
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
+          // required
+          name="address"
+          value={formData.address}
+          onChange={onChange}
         />
+        {errors.address && (
+          <p className="text-red-500 capitalize">{errors.address}</p>
+        )}
       </div>
-      <div>
+      <div className="mb-3">
         <label className="block capitalize text-xl tracking-widest mb-2">
           Phone
         </label>
         <input
           type="Phone"
-          className="block mb-4 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
-          required
+          className="block mb-3 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
+          // required
           placeholder="Phone"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          name="phone"
+          value={formData.phone}
+          onChange={onChange}
         />
+        {errors.phone && (
+          <p className="text-red-500 capitalize">{errors.phone}</p>
+        )}
       </div>
-      <div>
+      <div className="mb-3">
         <label className="block capitalize text-xl tracking-widest mb-2">
           email{" "}
         </label>
         <input
           type="email"
-          className="block mb-4 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
+          className="block mb-1 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
           placeholder="Email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          // required
+          name="email"
+          value={formData.email}
+          onChange={onChange}
         />
+        {errors.email && (
+          <p className="text-red-500 capitalize">{errors.email}</p>
+        )}
       </div>
       <div className="mb-4">
         <label className="block capitalize text-xl tracking-widest mb-2">
@@ -101,12 +149,16 @@ function SignUpFrom() {
         </label>
         <input
           type="password"
-          className="block mb-4 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
-          required
+          className="block mb-1 rounded-md px-4 py-2 outline-none bg-[#FFF7EA] border-slate-300 border-2 w-[20rem]"
+          // required
           placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          value={formData.password}
+          onChange={onChange}
         />
+        {errors.password && (
+          <p className="text-red-500 capitalize">{errors.password}</p>
+        )}
       </div>
       <button className="bg-[#FF9900] px-4 py-2 rounded-md text-white font-semibold tracking-widest text-xl col-span-2">
         SignUp
--- client/app/_store/userSlice.js
+++ client/app/_store/userSlice.js
@@ -6,6 +6,14 @@ const initialState = {
   user: {},
   Auth: isAuth(),
   loader: false,
+  errors: {
+    firstName: "",
+    lastName: "",
+    address: "",
+    phone: "",
+    email: "",
+    password: "",
+  },
 };
 const userSlice = createSlice({
   name: "User",
@@ -29,9 +37,45 @@ const userSlice = createSlice({
     Loader(state) {
       state.loader = true;
     },
+    SET_ERROR(state, action) {
+      switch (action.payload.type) {
+        case "ERROR_FIRSTNAME":
+          return {
+            ...state,
+            errors: { ...state.errors, firstName: action.payload.value },
+          };
+        case "ERROR_LASTNAME":
+          return {
+            ...state,
+            errors: { ...state.errors, lastName: action.payload.value },
+          };
+        case "ERROR_ADDRESS":
+          return {
+            ...state,
+            errors: { ...state.errors, address: action.payload.value },
+          };
+        case "ERROR_PHONE":
+          return {
+            ...state,
+            errors: { ...state.errors, phone: action.payload.value },
+          };
+        case "ERROR_EMAIL":
+          return {
+            ...state,
+            errors: { ...state.errors, email: action.payload.value },
+          };
+        case "ERROR_PASSWORD":
+          return {
+            ...state,
+            errors: { ...state.errors, password: action.payload.value },
+          };
+        default:
+          return state;
+      }
+    },
   },
 });
 
-export const { login, logout, getUser, Loader } = userSlice.actions;
+export const { login, logout, getUser, Loader, SET_ERROR } = userSlice.actions;
 
 export default userSlice.reducer;
