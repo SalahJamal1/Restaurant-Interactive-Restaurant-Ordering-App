65cda052a3b412713cb319f5af1fcf8991ec25a7
Salah <salah@Salahs-MacBook-Air.local>
2025-10-15 07:01:19 +0300

    devops

--- client/app/_lib/apiResto.js
+++ client/app/_lib/apiResto.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8080/api/v1/",
+  baseURL:
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "docker"
+      ? process.env.NEXT_PUBLIC_API_URL
+      : "http://localhost:8080/api/v1",
   withCredentials: true,
 });
 export async function getMenu(category) {
